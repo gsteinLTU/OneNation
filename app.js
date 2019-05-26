f239434e507bb9de4479b797f8bc5758e79191b4
Gordon Stein <gstein@ltu.edu>
2019-05-26 14:51:12 -0500

    Initial Setup

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log(`Listening on port ${port}`);
